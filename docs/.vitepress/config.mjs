ffc5018f4e3a846558e2eb9c60c4de83053008e7
adrianmiu <florin_miu@yahoo.com>
2025-10-06 09:56:46 +0300

    docs: added link to X profile

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
     },
     logo: '/logo.png',
     socialLinks: [
+      { icon: 'x', link: 'https://x.com/encolajs' },
       { icon: 'github', link: 'https://github.com/encolajs/encolajs-form-controller' },
     ],
     nav: [
